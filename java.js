85aecdb3f1e4870e016293c431c20f62326f8f25
jamaludeen <justjamal17@gmail.com>
2022-08-29 20:56:28 +0530

    java

---
+++ java.js
@@ -0,0 +1,49 @@
+//find addition
+{
+	let x=34;
+	let y=56;
+	let z=x+y;
+	document.write(z) 
+	document.write("<br>")
+}
+//calculate subraction
+{
+	let x=8;
+	let y=4;
+	let z=x-y;
+	document.write(z)
+	document.write("<br>")
+}
+//calculate multiplication
+{
+	let x=10;
+	let y=30;
+	let z=x*y;
+    document.write(z)
+	document.write("<br>")
+}
+//calculate division
+{
+	let x=5;
+	let y=15;
+	let z=x/y;
+    document.write(z)
+	document.write("<br>")
+}
+//Area of circle
+{
+	let a=Math.PI;
+	let r=Math.pow(5,2);
+    let A=a*r;
+	document.write(A)
+	document.write("<br>")
+}
+//Area of Triangle
+{
+	let a=1/2;
+	let b=4;
+	let h=4;
+	let A=a*b*h;
+	document.write(A)
+	document.write("<br>")
+}
